0ba0ba3beecb8924f52ba99060e98f0d147f576f
davidtom1 <davidtom@post.bgu.ac.il>
2025-11-15 13:56:47 +0200

    miluimnikim firebase

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCI6eaOk-l4FKIb3z7Gzd9oeUmw30k6xa8",
+  authDomain: "studio-9683312323-fd602.firebaseapp.com",
+  projectId: "studio-9683312323-fd602",
+  storageBucket: "studio-9683312323-fd602.firebasestorage.app",
+  messagingSenderId: "435539038042",
+  appId: "1:435539038042:web:0c7f088140ec0430c96d63"
+};
+
+export const app = initializeApp(firebaseConfig);
